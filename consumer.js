24594ca7426500829b1b9444556500b1c45ccf80
msmmanoj <manoj.malepati@acsicorp.com>
2021-10-07 17:13:17 +0530

    work queus

--- consumer.js
+++ consumer.js
@@ -1,26 +1,32 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect('amqp://localhost', function (error0, connection) {
     if (error0) {
         throw error0;
     }
-    connection.createChannel(function(error1, channel) {
+    connection.createChannel(function (error1, channel) {
         if (error1) {
             throw error1;
         }
 
-        var queue = 'hello';
+        var queue = 'work_queues';
 
         channel.assertQueue(queue, {
-            durable: false
+            durable: true
         });
-
+        channel.prefetch(1);
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-        channel.consume(queue,(msg) => {
+        channel.consume(queue, (msg) => {
+            var secs = msg.content.toString().split('.').length - 1;
+
             console.log(" [x] Received %s", msg.content.toString());
+            console.log("Processing");
+            setTimeout(function () {
+                console.log(" [x] Done");
+            }, secs * 1000)
         }, {
-            noAck: true
+            noAck: false
         })
     });
 });
--- producer.js
+++ producer.js
@@ -1,24 +1,26 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost',(err,connection) => {
-    if(err) {
+amqp.connect('amqp://localhost', (err, connection) => {
+    if (err) {
         throw err;
     }
-    connection.createChannel((err,channel) => {
-        if(err) {
+    connection.createChannel((err, channel) => {
+        if (err) {
             throw err;
         }
 
-        var queue = 'hello';
-        var msg = 'sample content';
+        var queue = 'work_queues';
+        var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-        channel.assertQueue(queue,{
-            durable:false,
+        channel.assertQueue(queue, {
+            durable: true,
         })
 
-        channel.sendToQueue(queue,Buffer.from(msg))
+        channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true,
+        })
 
-        console.log('[x] %s from',msg)
+        console.log('[x] %s sent', msg)
 
         setTimeout(() => {
             connection.close();
